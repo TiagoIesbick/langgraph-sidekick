d48d3ba7cf3b6ad3d8d61f3c6586fad705e496d7
TiagoIesbick <tiagoiesbick@gmail.com>
2025-10-16 20:46:10 -0300

    Remove main.py and update dependencies in pyproject.toml and uv.lock to include new packages and versions.

---
+++ tools/notifications.py
@@ -0,0 +1,28 @@
+from twilio.rest import Client
+from os import getenv
+from langchain.agents import Tool
+
+
+TWILIO_ACCOUNT_SID = getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = getenv("TWILIO_AUTH_TOKEN")
+TWILIO_WHATSAPP_FROM = getenv("TWILIO_WHATSAPP_FROM")
+TWILIO_WHATSAPP_TO = getenv("TWILIO_WHATSAPP_TO")
+
+print('twilio', TWILIO_ACCOUNT_SID)
+
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+def send_whatsapp(text: str) -> str:
+    msg = client.messages.create(
+        body=text,
+        from_=f'whatsapp:{TWILIO_WHATSAPP_FROM}',
+        to=f'whatsapp:{TWILIO_WHATSAPP_TO}'
+    )
+    return msg.sid
+
+whatsapp_tool = Tool(
+    name="send_whatsapp",
+    func=send_whatsapp,
+    description="Send a WhatsApp message via Twilio."
+)
